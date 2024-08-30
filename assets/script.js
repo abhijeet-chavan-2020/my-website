f82210bf48e757309755f81338b2758774406162
Abhijeet Chavan <aj.516147@gmail.com>
2024-08-31 04:54:44 +0530

    Fix

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// assets/js/script.js
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+});
